aa30e0f3ed867d3b0159f11f99decd613ef5ce1f
Rahulk8439 <rahul.sharma20030158@gmail.com>
2021-08-30 00:30:58 +0530

    contact update

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-//  js for top button 
+//  js for top button
 
 var mybutton = document.getElementById("myBtn");
 
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
-
-// js for toggle menu 
-
+// js for toggle menu
 
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.width = "100%";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNav").style.width = "0%";
 }
 
-
-// js for loader 
+// js for loader
 
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
-    loader.style.display = "none";
-})
-
-
+  loader.style.display = "none";
+});
 
 window.onscroll = function () {
-    if (window.pageYOffset > 50) {
-        document.getElementById('navbar').style.backgroundColor = "rgba(255, 82, 59, 0.9)";
-    }
-    else {
-        document.getElementById('navbar').style.backgroundColor = "transparent";
-    }
+  if (window.pageYOffset > 50) {
+    document.getElementById("navbar").style.backgroundColor =
+      "rgba(255, 82, 59, 0.9)";
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+  }
 };
-
 
 // products details
 
@@ -56,14 +55,14 @@ var ProductImg = document.getElementById("ProductImg");
 var SmallImg = document.getElementsByClassName("small-img");
 
 SmallImg[0].onclick = function () {
-    ProductImg.src = SmallImg[0].src;
-}
+  ProductImg.src = SmallImg[0].src;
+};
 SmallImg[1].onclick = function () {
-    ProductImg.src = SmallImg[1].src;
-}
+  ProductImg.src = SmallImg[1].src;
+};
 SmallImg[2].onclick = function () {
-    ProductImg.src = SmallImg[2].src;
-}
+  ProductImg.src = SmallImg[2].src;
+};
 SmallImg[3].onclick = function () {
-    ProductImg.src = SmallImg[3].src;
-}
+  ProductImg.src = SmallImg[3].src;
+};
